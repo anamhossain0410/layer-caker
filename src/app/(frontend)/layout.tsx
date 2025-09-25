1e85499941feb37963531840584f36bbb47daa18
anamhossain0410 <anam.hossain@multidots.com>
2025-09-25 17:35:50 +0600

    initial commit

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import { SanityLive } from '@/sanity/lib/live'
+
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -31,6 +33,8 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+        <SanityLive />
+
       </body>
     </html>
   );
